5222cbd4740509590b441146f8107d8ed2c388fa
JDC1492 <Jonathan_Cohen10@yahoo.com>
2020-12-29 12:58:55 -0500

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,22 @@
-// Code MovieReviews Here
+import React, {Component} from 'react';
+
+const MovieReview = ({display_title, mpaa_rating}) => {
+    return (
+   
+        <div key={display_title} className="review">
+            <h2>{display_title}</h2>
+            <p>{mpaa_rating}</p>      
+        </div>
+)}
+
+const MovieReviews = ({reviews}) => { 
+    return (
+        <div className="review-list">
+            {reviews.map(MovieReview)}
+        </div>
+    );
+}
+    // 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?api-key=Y5CQi2LIxH5bgkEVHadYrQPHUizosvNQ'
+
+export default MovieReviews;  
+     
